ed781cbd4764100c1f85fc61f4aaf9e8481bcc4e
alexandre <alexandre@cerebro.(none)>
2010-03-20 15:59:18 -0300

    MUdança nos feeds

--- scripts/add_rss.php
+++ scripts/add_rss.php
@@ -21,6 +21,14 @@ $sources = array(
     "BBC Portuguese" => "http://www.bbc.co.uk/portuguese/index.xml",
     "BBC Mundo" => "http://www.bbc.co.uk/mundo/index.xml",
     "BBC" => "http://www.bbc.co.uk/worldservice/full.xml",
+	"Lancenet"=>"http://www.lancenet.com.br/export/rss/noticias.xml",
+	"Uol"=>"http://rss.home.uol.com.br/index.xml",
+	"Estadao"=>"http://www.estadao.com.br/rss/ultimas.xml",
+	"FolhaSp"=>"http://feeds.folha.uol.com.br/folha/brasil/rss091.xml",
+	"IdgNow"=>"http://idgnow.uol.com.br/RSS2/index.html",
+	"R7"=>"http://www.r7.com/data/rss/brasil.xml",
+	"EBand"=>"http://www.band.com.br/rss/jornalismo.xml",
+	"JovemNerd"=>"http://jovemnerd.ig.com.br/feed/"
 );
 
 foreach ($sources as $name => $rss) {
