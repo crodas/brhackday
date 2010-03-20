aa44fc435b8d43739b14f5e18991e1ca72861c42
César D. Rodas <crodas@php.net>
2010-03-20 20:32:47 -0300

    - Upgraded ActiveMongo - Worked on lexicon model and lexicon2db.php

--- models/lexicon.php
+++ models/lexicon.php
@@ -6,4 +6,15 @@ class Lexicon extends ActiveMongo
     public $token;
     public $type;
     public $extra;
+
+    static $validates_presence_of = array(
+        'lang',
+        'token',
+        'type',
+    );
+
+    function setup()
+    {
+        $this->addIndex(array("lang" => 1));
+    }
 }
--- scripts/lexicon2db.php
+++ scripts/lexicon2db.php
@@ -1,30 +1,30 @@
 #!/usr/bin/php
 <?php
 require "../config.php";
-require "../libs/models.php";
+require "../models/lexicon.php";
 ini_set('memory_limit', '64M');
 
 $file = $argv[1];
 $lang = $argv[2];
 
 $content = bzdecompress(file_get_contents($argv[1]));
-$r = new Lexicon;
-$r->lang = $lang;
-$r->deleteAll();
 
+Lexicon::drop();
+ActiveMongo::Install();
+
+$r = new Lexicon;
 foreach (explode("\n", $content) as $line) {
     $info = explode(" ", $line);
     if (count($info) < 2) {
         continue;
     }
-    $r = new Lexicon;
+    $r->reset();
     $r->lang  = $lang;
     $r->token = strtolower($info[0]);
     $r->type  = $info[1];
     if (count($info) > 2) {
         $r->extra = array_slice($info, 2);
     }
-    $r->save();
-    unset($r);
+    $r->save(false);
 }
 
