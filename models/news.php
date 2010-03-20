5e7328bb855e0872f4ae0828ba696386acc8a3be
César D. Rodas <crodas@php.net>
2010-03-20 16:01:57 -0300

    Working on the crawler

--- models/news.php
+++ models/news.php
@@ -3,4 +3,28 @@
 class News extends ActiveMongo
 {
     public $source;
+    public $title;
+    public $content;
+    public $url;
+    public $processed;
+    public $languages;
+
+    static $validates_presence_of = array(
+        'source',
+        'title', 
+        'content',
+        'url'
+    );
+
+    function before_create(&$obj)
+    {
+        /* Putting the new News in our processing queue */
+        $obj['processed'] = false;
+    }
+    
+    function setup()
+    {
+        $this->addIndex(array("source" => 1));
+        $this->addIndex(array("url" => 1), array("unique" => 1));
+    }
 }
